3a4fc04f7a37048e9daf302cef2446cc0378d176
Maxwell <Maxwellbarboza@hotmail.com>
2025-02-07 21:28:08 -0300

    Foi criado o service de transaction e exception

--- src/main/java/com/maxwell/picpay_desafio_backend/transaction/TransactionService.java
+++ src/main/java/com/maxwell/picpay_desafio_backend/transaction/TransactionService.java
@@ -1,26 +1,29 @@
 package com.maxwell.picpay_desafio_backend.transaction;
 
+import com.maxwell.picpay_desafio_backend.exception.InvalidTransactionException;
+import com.maxwell.picpay_desafio_backend.wallet.Wallet;
 import com.maxwell.picpay_desafio_backend.wallet.WalletRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.maxwell.picpay_desafio_backend.wallet.WalletType;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class TransactionService {
 
-    @Autowired
-    private TransactionRepository transactionRepository;
-    @Autowired
-    private WalletRepository walletRepository;
+
+    private final TransactionRepository transactionRepository;
+    private final WalletRepository walletRepository;
 
     public TransactionService(TransactionRepository transactionRepository, WalletRepository walletRepository) {
         this.transactionRepository = transactionRepository;
         this.walletRepository = walletRepository;
     }
 
+    @Transactional
     public Transaction create(Transaction transaction){
 
         // 1- Validar
-        validate()
+        validate(transaction);
 
         // 2 - criar a transação
         var newTransaction = transactionRepository.save(transaction);
@@ -29,13 +32,26 @@ public class TransactionService {
         var wallet = walletRepository.findById(transaction.payer()).get();
         walletRepository.save(wallet.debit(transaction.value()));
 
-
         // 4 - chamar serviços externos
-
         return newTransaction;
     }
 
-    private void validate(Transaction transaction){
+    // 1- the payer has a common wallet
+    // 2- the payer has enough balance
+    // 3 - the payer is not the payee
 
+    private void validate(Transaction transaction){
+        walletRepository.findById(transaction.payee())
+                .map(payee -> walletRepository.findById(transaction.payer())
+                    .map(payer -> isTransactionValid(transaction, payer) ? transaction : null)
+                    .orElseThrow(() -> new InvalidTransactionException("Invalid transaction - %s".formatted(transaction))))
+                .orElseThrow(() -> new InvalidTransactionException("Invalid transaction - %s".formatted(transaction)));
+
+    }
+
+    private boolean isTransactionValid(Transaction transaction, Wallet payer) {
+        return payer.type() == WalletType.COMUM.getValue() &&
+                payer.balance().compareTo(transaction.value()) >= 0 &&
+                !payer.id().equals(transaction.payee());
     }
 }
